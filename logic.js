83d728dacc990f30a8561a7467c687c996772cf9
waylandsingh <wmsingh@ucdavis.edu>
2021-07-22 17:03:37 -0700

    Basic Listing of books in myLibrary & addBooktoLib

--- logic.js
+++ logic.js
@@ -5,13 +5,24 @@ function Book(title, author, pages, readStatus) {
     this.readStatus = readStatus
 }
 
-Book.prototype.info = (this) => {
+Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages - ${this.readStatus}`
 }
 
 let myLibrary = []
 
-function addBookToLib() {
+function addBookToLib(lib, title, author, pages, readStatus) {
     //stuff here
-    
+    myLibrary.push(new Book(title, author, pages, readStatus))
 }
+
+addBookToLib(myLibrary, 'The Hobbit', 'Tolkien', '600', 'Finished Reading')
+addBookToLib(myLibrary, 'The Count of Monte Cristo', 'Dumas', '1400', 'Finished Reading')
+
+infoSection = document.querySelector('#list-info')
+
+myLibrary.forEach((book) => {
+    let d = document.createElement('div')
+    d.innerText = book.info()
+    infoSection.appendChild(d)
+})
