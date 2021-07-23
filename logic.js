e1b27b32fe4766311ae1c43184556ac13c47faa0
waylandsingh <wmsingh@ucdavis.edu>
2021-07-22 22:26:11 -0700

    click to add eventHandler added + rerender

--- logic.js
+++ logic.js
@@ -19,10 +19,35 @@ function addBookToLib(lib, title, author, pages, readStatus) {
 addBookToLib(myLibrary, 'The Hobbit', 'Tolkien', '600', 'Finished Reading')
 addBookToLib(myLibrary, 'The Count of Monte Cristo', 'Dumas', '1400', 'Finished Reading')
 
-infoSection = document.querySelector('#list-info')
+// select the display area
+let infoSection = document.querySelector('#list-info')
+function render() {
+    infoSection.innerText = ''
+    myLibrary.forEach((book) => {
+        let d = document.createElement('div')
+        // maybe rework to have more semantic structure
+        // think about CSS/display, size, class of the div!
+        d.innerText = book.info()
+        infoSection.appendChild(d)
+    })
+}
 
-myLibrary.forEach((book) => {
-    let d = document.createElement('div')
-    d.innerText = book.info()
-    infoSection.appendChild(d)
+// render on page load
+render()
+
+// function to run when 
+// event listener for clicks on adding a book
+// on click, console.log all of the info in the inputs
+//find the button ->
+document.querySelector('#add-button').addEventListener('click', ()=>{
+    let formFields = document.querySelectorAll("#add-book-details input")
+    let info = {}
+    formFields.forEach((f)=>{
+        info[f.name] = f.value
+    })
+    
+    addBookToLib(myLibrary, ...Object.values(info))
+    render()
+    // console.log(info)
 })
+
